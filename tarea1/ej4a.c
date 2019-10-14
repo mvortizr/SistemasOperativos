2890c40d66d44c129f66f7267fbb7b7daf30b2c0
Maria V <mariatua2@gmail.com>
2019-10-14 19:34:37 -0400

    ej4a un poquito mejor

--- tarea1/ej4a.c
+++ tarea1/ej4a.c
@@ -9,8 +9,40 @@ int main (){
 	int child_count=0;
 
 	//Defino funciones en este contexto para usar child_count como var global
-	void handle_signal(int signal); 
-	void createChildWithExec();
+	void createChildWithExec(){
+		if(fork()==0) {
+		    printf("[PROCESO HIJO] PID: %d, PID padre: %d, PID grupo:%d\n", getpid(), getppid(), getpgrp());
+		    char *args[] = {"xcalc", NULL};
+		    execvp("/usr/bin/xcalc", args);
+		} else {
+		    child_count++;
+		   	printf("[PROCESO PADRE] PID: %d, Hijo(s): %i, PID grupo:%d\n", getpid(), child_count,getpgrp());    	
+		}
+	}
+
+
+	void signal_controller(int signal) {
+    
+	    const char *signal_name;
+
+	    switch (signal) {
+	        case SIGHUP:
+	            signal_name = "SIGHUP";
+	         	createChildWithExec();
+	            break;
+	        case SIGTERM:
+	            signal_name = "SIGTERM";
+	            kill(0,15);
+	            break;
+	        default:
+	            fprintf(stderr, "Error: %d\n", signal);
+	            return;
+	    }
+
+   		printf("Se ha recibido una señal %s\n\n", signal_name);
+
+
+	}
 
 
 	printf("[PROCESO] PID: %d\n", getpid());
@@ -18,10 +50,14 @@ int main (){
 	//Asigno mi controlador al manejador de signals
 	sa.sa_handler = &signal_controller;
 
-	
+
 	//Manejo de errores
     if (sigaction (SIGHUP, &sa, NULL) == -1) {
         perror("Error: no se pudo manejar SIGHUP");
+    }
+
+    if (sigaction (SIGTERM, &sa, NULL) == -1) {
+        perror("Error: no se pudo manejar SIGTERM");
     }
 
     //Ciclo para dejarlo "escuchando"
@@ -32,30 +68,5 @@ int main (){
 	return 0;
 }
 
-void createChildWithExec(){
-	if(fork()==0) {
-	    printf("[PROCESO HIJO] PID: %d, PID padre: %d\n", getpid(), getppid());
-	    char *args[] = {"xcalc", NULL};
-	    execvp("/usr/bin/xcalc", args);
-	} else {
-	    child_count++;
-	   	printf("[PROCESO PADRE] PID: %d, tengo %i hijos\n", getpid(), child_count);    	
-	}
-}
 
-void signal_controller(int signal) {
-    
-    const char *signal_name;
 
-    switch (signal) {
-        case SIGHUP:
-            signal_name = "SIGHUP";
-         	createChildWithExec();
-            break;
-        default:
-            fprintf(stderr, "Error: %d\n", signal);
-            return;
-    }
-
-    printf("Se ha recibido una señal %s\n\n", signal_name);
-}
