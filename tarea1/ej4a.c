bb0691a81152270d37aa42fa19915c3e5d2a6724
Maria V <mariatua2@gmail.com>
2019-10-14 18:29:50 -0400

    ej4a abriendo xcalc en proceso hijo y contando procesos

--- tarea1/ej4a.c
+++ tarea1/ej4a.c
@@ -2,32 +2,24 @@
 #include <signal.h> 
 #include <unistd.h>
 
-void signal_controller(int signal) {
-    const char *signal_name;
-
-    switch (signal) {
-        case SIGHUP:
-            signal_name = "SIGHUP";
-            break;
-        default:
-            fprintf(stderr, "Error con senal: %d\n", signal);
-            return;
-    }
-
-    printf("Me mandaste un%s\n\n", signal_name);
-}
-
-
-
 int main (){
 	
+	//Defino variables
 	struct sigaction sa;
+	int child_count=0;
 
-	fprintf("[PROCESO] PID: %d\n", getpid());
+	//Defino funciones en este contexto para usar child_count como var global
+	void handle_signal(int signal); 
+	void createChildWithExec();
 
-	 sa.sa_handler = &signal_controller;
+
+	printf("[PROCESO] PID: %d\n", getpid());
+
+	//Asigno mi controlador al manejador de signals
+	sa.sa_handler = &signal_controller;
 
 	
+	//Manejo de errores
     if (sigaction (SIGHUP, &sa, NULL) == -1) {
         perror("Error: no se pudo manejar SIGHUP");
     }
@@ -38,4 +30,32 @@ int main (){
     }
 
 	return 0;
+}
+
+void createChildWithExec(){
+	if(fork()==0) {
+	    printf("[PROCESO HIJO] PID: %d, PID padre: %d\n", getpid(), getppid());
+	    char *args[] = {"xcalc", NULL};
+	    execvp("/usr/bin/xcalc", args);
+	} else {
+	    child_count++;
+	   	printf("[PROCESO PADRE] PID: %d, tengo %i hijos\n", getpid(), child_count);    	
+	}
+}
+
+void signal_controller(int signal) {
+    
+    const char *signal_name;
+
+    switch (signal) {
+        case SIGHUP:
+            signal_name = "SIGHUP";
+         	createChildWithExec();
+            break;
+        default:
+            fprintf(stderr, "Error: %d\n", signal);
+            return;
+    }
+
+    printf("Se ha recibido una señal %s\n\n", signal_name);
 }
