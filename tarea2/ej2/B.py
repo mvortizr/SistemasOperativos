0401f5c35e5cd75531a1f33e97f710338121cc23
Gustavo Quintana <32227166+GJQP@users.noreply.github.com>
2019-11-21 19:58:42 -0400

    Ejercicio2 utilizando mkfifo dentro

---
+++ tarea2/ej2/B.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import os, sys, random, string, sysv_ipc, posix
+
+seed = sys.argv[1]
+
+random.seed(seed) 
+
+def decrypt(encrypted):
+    key = string.ascii_lowercase
+    encrypted = encrypted.split()
+    key = ''.join(random.choice(key) for i in range(len(encrypted)))
+    decrypted = [ chr(int(a, 2) ^ ord(b)) for (a,b) in zip(encrypted, key) ]
+    decrypted = "".join(decrypted)
+    return decrypted
+
+memory = sysv_ipc.SharedMemory(1)
+
+nombre_pipe = memory.read()
+memory.detach()
+while 1:
+    pypethon = open(nombre_pipe,'r')
+    mensaje = pypethon.read()
+    pypethon.close()
+    if decrypt(mensaje) == '': 
+        print("Se ha cerrado A")
+        break
+    print("Mensaje encriptado: " + mensaje)
+    print("Mensaje original: " + decrypt(mensaje))
+    
+
